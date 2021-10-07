7621f094697576262acf6233240d78690913f679
Michael DeSantis <m.desantis628@gmail.com>
2021-10-06 20:13:21 -0400

    Menu items are objects

---
+++ src/menu.js
@@ -0,0 +1,157 @@
+let menu = [{
+        'name': 'Stripclub Wedge of Jarslberg',
+        'description': 'Aged at The Bing',
+        'price': '$15.99',
+        'course': 'appetizer'
+    },
+    {
+        'name': 'Valentina\'s Egg Beaters',
+        'description': 'Served with Tabasco en flambé',
+        'price': '$6.99',
+        'course': 'main'
+    },
+    {
+        'name': 'Adrana\'s Cheez Dogz',
+        'description': '2 Weiners and 2 Buns served beneath 1 full can of Cheez Whiz',
+        'price': '$11.99',
+        'course': 'main'
+    },
+    {
+        'name': 'Sil\'s Bread',
+        'description': 'Fresh from the Bread Museum',
+        'price': '$4.99/basket',
+        'course': 'appetizer'
+    },
+    {
+        'name': 'Tony\'s Fucking Lo Mein',
+        'description': 'Generous Portion for guaranteed leftovers',
+        'price': '$7.99',
+        'course': 'main'
+    },
+    {
+        'name': 'Sushi with Cayenne Peppah',
+        'description': 'Perfect for a Date Night',
+        'price': '$11.99',
+        'course': 'main'
+    },
+    {
+        'name': 'Livia\'s Fuckin\' Ziti',
+        'description': 'Birthday Party sized tray',
+        'price': '$29.99',
+        'course': 'main'
+    },
+    {
+        'name': 'Lincoln Log Sandwich',
+        'description': 'Guaranteed to turn frowns upside down',
+        'price': '$4.99',
+        'course': 'main'
+    },
+    {
+        'name': 'New Jersey\'s Greatest Sangwich',
+        'description': 'Only 1 available daily whenever Christopher feels like delivering it',
+        'price': '$9.99',
+        'course': 'main'
+    },
+    {
+        'name': 'Scrambled Eggs with Sour Cream',
+        'description': 'So good, this could be your last meal!',
+        'price': '$5.99',
+        'course': 'main'
+    },
+    {
+        'name': 'Artie\'s Zuppa di Mussels',
+        'description': 'Fresh',
+        'price': '$25.99',
+        'course': 'main'
+    },
+    {
+        'name': 'Junior\'s Peppahs and Eggs Sandwich',
+        'description': 'If you don\'t order this, you\'ll wish you did!',
+        'price': '$9.99',
+        'course': 'main'
+    },
+    {
+        'name': 'Grilled Cheese',
+        'description': 'Prepared on an antique jailhouse radiator',
+        'price': '$9.99',
+        'course': 'main'
+    },
+    {
+        'name': 'Macaroni and Gravy',
+        'description': 'Commendatori!',
+        'price': '$14.99',
+        'course': 'main'
+    },
+    {
+        'name': 'Quail a la Bucco',
+        'description': 'Stuffed with fennel',
+        'price': '$27.99',
+        'course': 'main'
+    },
+    {
+        'name': 'Giant Bowl of Ice Cream',
+        'description': 'Served with a spatula for consumption ease',
+        'price': '$8.99',
+        'course': 'dessert'
+    },
+    {
+        'name': 'Motherfuckin\' Goddamn Orange Peel Beef',
+        'description': 'Make sure you check your order',
+        'price': '$11.99',
+        'course': 'main'
+    },
+    {
+        'name': 'Janice\'s Carmela\'s Lasagna',
+        'description': 'Little bits of sweet sausage, layer of basil leaves right under the cheese',
+        'price': '$19.99',
+        'course': 'main'
+    },
+    {
+        'name': 'Burrata and Green Beans',
+        'description': 'It\'s nutty',
+        'price': '$10.99',
+        'course': 'appetizer'
+    },
+    {
+        'name': 'Meadow\'s Summertime Hot Chili',
+        'description': 'It\'s really good chili',
+        'price': '$4.99',
+        'course': 'main'
+    },
+    {
+        'name': 'Hugh\'s Favorite Maple Walnut Icebox Cake',
+        'description': 'Only available during peak hours',
+        'price': '$8.99',
+        'course': 'dessert'
+    },
+    {
+        'name': 'Gloria Trillo\'s London Broil',
+        'description': 'Served mid-air',
+        'price': '$22.99',
+        'course': 'main'
+    },
+    {
+        'name': 'The Gabagool',
+        'description': 'So good, you\'ll faint!',
+        'price': '$8.99',
+        'course': 'appetizer'
+    },
+    {
+        'name': 'Fresh Mozzarella',
+        'description': 'The secret is in the ashes',
+        'price': '$8.99',
+        'course': 'appetizer'
+    },
+    {
+        'name': 'Junior\'s Favorite Spicy Chicken',
+        'description': 'Worth leaving the house for',
+        'price': '$14.99',
+        'course': 'main'
+    },
+    {
+        'name': 'The Last Supper Onion Rings',
+        'description': 'Don\'t Stop Believing',
+        'price': '$10.99',
+        'course': 'appetizer'
+    }
+]
