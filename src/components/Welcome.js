050115069cd11becf3edb5628685da49cc7addf2
crono628 <84817352+crono628@users.noreply.github.com>
2022-06-24 12:52:11 -0400

    started welcome page. experimenting with tailwind.

---
+++ src/components/Welcome.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Welcome = () => {
+  return (
+    <div className="title container max-w-2xl mx-auto bg-gray-200 rounded-xl shadow border p-5 m-10">
+      <p className="text-9xl text-center text-gray-700 font-bold mb-5">
+        Nuovo Vesuvio
+      </p>
+      <p className=" text-center text-gray-500 text-2xl">
+        You'll taste the crispness of the beans and the smoothness of the cheese
+      </p>
+    </div>
+  );
+};
+
+export default Welcome;
